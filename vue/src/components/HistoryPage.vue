984fe6faf966040081e2167881ff0af90f887e93
AP <afonsocpimenta04@gmail.com>
2024-12-06 22:53:57 +0000

    save before merge

--- vue/src/components/HistoryPage.vue
+++ vue/src/components/HistoryPage.vue
@@ -7,26 +7,31 @@
         <table class="min-w-full table-auto border-collapse">
           <thead>
             <tr class="bg-gray-100">
-              <th class="border-b px-4 py-2">Game Date</th>
-              <th class="border-b px-4 py-2">Board Size</th>
-              <th class="border-b px-4 py-2">Status</th>
+              <th class="border-b px-4 py-2">Game Id</th>
+              <th class="border-b px-4 py-2">Creator Id</th>
+              <th class="border-b px-4 py-2">Winner Id</th>
+              <th class="border-b px-4 py-2">Game Type</th>
+              <th class="border-b px-4 py-2">Game Status</th>
+              <th class="border-b px-4 py-2">Start Time</th>
+              <th class="border-b px-4 py-2">End Time</th>
               <th class="border-b px-4 py-2">Total Time</th>
-              <th class="border-b px-4 py-2">Turns</th>
-              <th class="border-b px-4 py-2">Players</th>
-              <th class="border-b px-4 py-2">Winner</th>
+              <th class="border-b px-4 py-2">Board Size</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="game in games" :key="game.id">
-              <td class="border-b px-4 py-2">{{ formatDate(game.start_date) }}</td>
-              <td class="border-b px-4 py-2">{{ game.board_size }}</td>
-              <td class="border-b px-4 py-2">{{ game.status }}</td>
-              <td class="border-b px-4 py-2">{{ game.total_time }} min</td>
-              <td class="border-b px-4 py-2">{{ game.turns }}</td>
-              <td class="border-b px-4 py-2">{{ game.players.join(', ') }}</td>
-              <td class="border-b px-4 py-2">{{ game.winner }}</td>
+                <td class="border-b px-4 py-2">{{ game.id }}</td>
+                <td class="border-b px-4 py-2">{{ game.creator_id }}</td>
+                <td class="border-b px-4 py-2">{{ game.winner_id }}</td>
+                <td class="border-b px-4 py-2">{{ game.type }}</td>
+                <td class="border-b px-4 py-2">{{ game.status }}</td> 
+                <td class="border-b px-4 py-2">{{ game.began_at }}</td>
+                <td class="border-b px-4 py-2">{{ game.ended_at }}</td>
+                <td class="border-b px-4 py-2">{{ game.total_time }}</td>
+                <td class="border-b px-4 py-2">{{ game.board_cols }}x{{ game.board_rows }}</td>               
+
             </tr>
-          </tbody>
+            </tbody>
         </table>
       </div>
   
@@ -41,16 +46,14 @@
   import { ref, onMounted } from 'vue';
   import { useRouter } from 'vue-router';
   import axios from 'axios';
-  
 
   const games = ref([]);
   
-
-
   const fetchGameHistory = async () => {
     try {
-      const response = await axios.get('/api/games/history');
-      games.value = response.data;
+      const response = await axios.get('/users/me/history');
+      games.value = response.data.data;
+      console.log("Games:" . games)
     } catch (error) {
       console.error('Error fetching game history:', error);
     }
