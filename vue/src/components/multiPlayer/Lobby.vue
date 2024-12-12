21e541914dc72fe15b51ddc946b3df6a45605c6d
AP <afonsocpimenta04@gmail.com>
2024-12-12 00:02:23 +0000

    popover board size multiplayer

--- vue/src/components/multiPlayer/Lobby.vue
+++ vue/src/components/multiPlayer/Lobby.vue
@@ -8,14 +8,25 @@ import {
 } from '@/components/ui/card'
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
+import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover';
 import ListGamesLobby from './ListGamesLobby.vue'
 import { useLobbyStore } from '@/stores/lobby'
 import { useAuthStore } from '@/stores/auth';
 import { useGamesStore } from '@/stores/games';
+import { useBoardStore } from '@/stores/board';
+import { ref } from 'vue';
 
 const storeGames = useGamesStore()
 const storeAuth = useAuthStore()
 const storeLobby = useLobbyStore()
+const storeBoard = useBoardStore()
+
+const popoverOpen = ref(false);
+
+const selectBoard = (boardId) => {
+    storeLobby.addGame(boardId)
+    popoverOpen.value = false;
+}
 </script>
 
 <template>
@@ -27,9 +38,18 @@ const storeLobby = useLobbyStore()
         </CardHeader>
         <CardContent v-if="storeAuth.user" class="p-4 flex-1 overflow-hidden flex flex-col">
             <div class="py-2">
-                <Button @click="storeLobby.addGame(1)"> <!-- TODO fazer com que o user escolha o board -->
-                    New Game
-                </Button>
+                <Popover v-model:open="popoverOpen">
+                    <PopoverTrigger>
+                        <Button>
+                            New Game
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent class="w-48">
+                        <div v-for="board in storeBoard.boards" :key="board.id" @click="selectBoard(board.id)" class="cursor-pointer p-2 hover:bg-gray-200">
+                            {{ board.board_cols }}x{{ board.board_rows }}
+                        </div>
+                    </PopoverContent>
+                </Popover>
             </div>
             <ScrollArea class="flex-1 overflow-auto">
                 <div v-if="storeLobby.totalGames > 0">
