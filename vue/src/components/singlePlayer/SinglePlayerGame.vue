7fdbd4f709e5a67af0ebde9b8a5caedc3d76754e
patinhooh <patinhooh88@outlook.com>
2024-12-14 23:08:40 +0000

    Fix game exit, update interrupt game

--- vue/src/components/singlePlayer/SinglePlayerGame.vue
+++ vue/src/components/singlePlayer/SinglePlayerGame.vue
@@ -1,23 +1,23 @@
 <script setup>
-import { onMounted, onUnmounted, onBeforeUnmount, watch, ref, h } from "vue";
-import { useMemoryGame } from "@/components/game/memoryGame";
-import { useGameStore } from "@/stores/game";
-import { useAuthStore } from "@/stores/auth";
+import { onMounted, onUnmounted, onBeforeUnmount, watch, ref, h } from 'vue'
+import { useMemoryGame } from '@/components/game/memoryGame'
+import { useGameStore } from '@/stores/game'
+import { useAuthStore } from '@/stores/auth'
 
-import { useToast } from "@/components/ui/toast/use-toast";
+import { useToast } from '@/components/ui/toast/use-toast'
 import { ToastAction } from '@/components/ui/toast'
 
-import Top5Card from "@/components/game/Top5Card.vue";
-import GameStatusCard from "@/components/game/GameStatusCard.vue";
-import MemoryGame from "@/components/game/MemoryGame.vue";
+import Top5Card from '@/components/game/Top5Card.vue'
+import GameStatusCard from '@/components/game/GameStatusCard.vue'
+import MemoryGame from '@/components/game/MemoryGame.vue'
 
-import router from "@/router";
+import router from '@/router'
 
 const { toast } = useToast()
 
-const storeGame = useGameStore();
-const storeAuth = useAuthStore();
-const { 
+const storeGame = useGameStore()
+const storeAuth = useAuthStore()
+const {
   cards,
   isGameOver,
   flipCard,
@@ -26,77 +26,81 @@ const {
   pairsFound,
   getCurrentTime,
   getTotalTime
-} = useMemoryGame(storeGame.board);
+} = useMemoryGame(storeGame.board)
 
-const currentTime = ref("")
+const currentTime = ref('')
 const updateCurrentTime = () => {
-  currentTime.value = getCurrentTime();
-};
-const updateTimeInterval = setInterval(updateCurrentTime, 100);
+  currentTime.value = getCurrentTime()
+}
+const updateTimeInterval = setInterval(updateCurrentTime, 100)
+let gameStarted = false
 
 const createGame = async () => {
-  console.log("Current board:", storeGame.board.id);
-  console.log("storeAuth.user = " + storeAuth.user)
-  
-  if(storeAuth.user){
-    console.log("insert game")
+  gameStarted = true
+  console.log('Current board:', storeGame.board.id)
+  console.log('storeAuth.user = ' + storeAuth.user)
+
+  if (storeAuth.user) {
+    console.log('insert game')
     await storeGame.insertGame({
-      type:"S",
-      status:"PL",
-      board_id:storeGame.board.id
+      type: 'S',
+      status: 'PL',
+      board_id: storeGame.board.id
     })
   }
 }
 
 const restartGame = async () => {
-  if (storeGame.game.id && !isGameOver.value && storeAuth.user){
-    await storeGame.updateGame({status: "I"})
+  gameStarted = false
+  if (storeGame.game.id && !isGameOver.value && storeAuth.user) {
+    await storeGame.updateGame({ status: 'I' })
   }
   resetGame()
 }
 
-watch(
-  isGameOver,
-  async (newValue) => {
-    if (newValue && storeAuth.user){
-      await storeGame.updateGame({
-        status: "E",
-        total_time: getTotalTime(),//change to actual value
-        total_turns_winner: totalTurns.value,//change to actual value
-      })
-    }
-    storeGame.reloadRequestTop5 = !storeGame.reloadRequestTop5
-    storeGame.reloadRequestMemoryGame = !storeGame.reloadRequestMemoryGame
+watch(isGameOver, async (newValue) => {
+  if (newValue && storeAuth.user) {
+    await storeGame.updateGame({
+      status: 'E',
+      total_time: getTotalTime(),
+      total_turns_winner: totalTurns.value
+    })
   }
-)
-onMounted(()=>{
-  if (!storeGame.board.id){
-    router.push("/singleplayer");
-    return;
+  storeGame.reloadRequestTop5 = !storeGame.reloadRequestTop5
+  storeGame.reloadRequestMemoryGame = !storeGame.reloadRequestMemoryGame
+})
+onMounted(() => {
+  if (!storeGame.board.id) {
+    router.push('/singleplayer')
+    return
   }
-  if(!storeAuth.user){
+  if (!storeAuth.user) {
     toast({
-      title: "Log in to Enhance Your Experience",
-      description: "Log in to ensure your records appear on the Scoreboard and stay saved. It's quick and easy!",
-      action: h(ToastAction, {
-        altText: "Log in to access additional features",
-        onclick: () => {
-          router.push("/testers/laravel");
+      title: 'Log in to Enhance Your Experience',
+      description:
+        "Log in to ensure your records appear on the Scoreboard and stay saved. It's quick and easy!",
+      action: h(
+        ToastAction,
+        {
+          altText: 'Log in to access additional features',
+          onclick: () => {
+            router.push('/testers/laravel')
+          }
         },
-      }, {
-        default: () => "Log In",
-      }),
-    });
+        {
+          default: () => 'Log In'
+        }
+      )
+    })
   }
 })
 
-onBeforeUnmount(() => clearInterval(updateTimeInterval));
-onUnmounted(async ()=>{
-  //clearInterval(timer);
-  console.log("isGameOver.value = " + isGameOver.value)
-  console.log("storeAuth.user = " + storeAuth.user)
-  if (!isGameOver.value && storeAuth.user){
-    await storeGame.updateGame({status: "I"})
+onBeforeUnmount(() => clearInterval(updateTimeInterval))
+onUnmounted(async () => {
+  console.log('isGameOver.value = ' + isGameOver.value)
+  console.log('storeAuth.user = ' + storeAuth.user)
+  if (storeAuth.user && gameStarted && !isGameOver.value) {
+    await storeGame.updateGame({ status: 'I' })
   }
 })
 </script>
@@ -109,21 +113,18 @@ onUnmounted(async ()=>{
         :is-game-over="isGameOver"
         :pairs-found="pairsFound"
         :time="currentTime"
-        :total-pairs="cards.length/2"
+        :total-pairs="cards.length / 2"
         :turns="totalTurns"
         @restart="restartGame"
       />
-        
+
       <!-- Main Memory Game Section -->
       <div class="flex flex-col items-center w-full md:w-3/4">
         <h1 class="text-2xl font-bold mt-6 mb-4">Memory Game</h1>
-        <MemoryGame 
-         :cards="cards"
-         :flipCard="flipCard"
-         @gameStarted="createGame"/>
+        <MemoryGame :cards="cards" :flipCard="flipCard" @gameStarted="createGame" />
       </div>
     </div>
     <!-- Leaderboard as Sticky Navigation Panel on the Right -->
-    <Top5Card class="mt-5 mx-5" :board="storeGame.board"/>
+    <Top5Card class="mt-5 mx-5" :board="storeGame.board" />
   </div>
 </template>
