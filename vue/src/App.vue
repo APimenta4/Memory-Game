9fa569a37739c25cb91b6dbf08730cd28088bdec
AP <afonsocpimenta04@gmail.com>
2024-12-21 13:29:34 +0000

    change brain coins location

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -273,6 +273,14 @@ onMounted(async () => {
           >
             Register
           </RouterLink>
+
+          <div v-if="storeAuth.user" style="display: flex; align-items: center">
+            <span style="font-size: 24px; margin-right: 8px">🧠</span>
+            <span style="font-size: 18px">{{ storeAuth.user.brain_coins_balance }}</span>
+          </div>
+
+          <BuyCoins> </BuyCoins>
+
           <!-- Profile Image -->
           <img
             v-if="storeAuth.user"
@@ -281,13 +289,6 @@ onMounted(async () => {
             :src="storeAuth.userPhotoUrl"
             alt="Profile Picture"
           />
-
-          <div v-if="storeAuth.user" style="display: flex; align-items: center">
-            <span style="font-size: 24px; margin-right: 8px">🧠</span>
-            <span style="font-size: 18px">{{ storeAuth.user.brain_coins_balance }}</span>
-          </div>
-
-          <BuyCoins> </BuyCoins>
 
           <!-- Submenu (Conditional Rendering) -->
           <div
